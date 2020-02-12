f67becd4af855bccec7ab96f4e69b792d5bc0644
PRIYADHARSHINI <PRIYADHARSHINI@LAPTOP-CMFBOBNE>
2020-02-12 18:42:12 +0530

    Add project files.

---
+++ WebApplication1/Controllers/RegisterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public string Index()
+        {
+            return "sunday holiday";
+        }
+    }
+}
